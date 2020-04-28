3f84b0ba30155ece545ea8d798e62d4fa617e645
Jaedu Cho <chojd7@gmail.com>
2020-04-27 20:49:17 -0700

    first commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Jaedu Cho on 4/27/20.
+//  Copyright © 2020 Jaedu Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
